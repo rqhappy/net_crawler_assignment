ff5c4f36173094c583e803480a1f6edfc6846467
rqhappy <627814418@qq.com>
2017-11-18 15:28:02 +0800

    试着运行一下

--- main.c
+++ main.c
@@ -1,30 +1,53 @@
 #include <stdio.h>
 #include <sys/socket.h>
 #include "net_util.h"
-#include "fsm.h"
-#include "bloom.h"
-#include "hash.h"
-
-#include <stdio.h>
+//#include "fsm.h"
+//#include "bloom.h"
+//#include "hash.h"
+#include "thread_pool.h"
+#include "thread_work.h"
+#include "linked_queue.h"
 
 int main(int argc, const char * argv[]) {
+    
+    int thread = 4;
+    thread_pool_t pool =  thread_pool_create(thread);
+    l_queue q= init_queue();
+    sock_d* socks =  sock_init("8080", "127.0.0.1");
+    var_init();
+    struct arguments *arg = (struct arguments*)malloc(sizeof(struct arguments));
+    arg->number = thread;
+    arg->socks = socks;
+    arg->q = q;
+    
+    for (int i = 0; i < thread; i++) {
+        thread_pool_add_task(pool, (void*)t_task, (void*)arg);
+    }
+    puts("press enter to terminate ...");
+    getchar();
+    /*
     // insert code here...
-    /*if(argc < 4)
+    if(argc < 4)
      {
      printf("Usage: crawler (ip_address) (port) (url.txt)\n");
      return 0;
-     }:*/
+     }:
     //char ip[50];
     //hostname_to_ip(argv[2],ip);
     sock_d sock = connection("8080","127.0.0.1");
-    send_req(sock, "/", NULL);
+    CONN_STAT index_state = (CONN_STAT)malloc(sizeof(CONN_STAT));
+    index_state->host = "Host: 127.0.0.1\n";
+    index_state->host_len = strlen(index_state->host);
+    index_state->refer = NULL;
+    index_state->refer_len = 0;
+    send_req(sock, "/", index_state);
     int len = analysis_head(sock);
     if(len == -1)
     {
         printf("request fail");
     }
     BLOOM *bloom = bloom_init(bloom_size,10,simple_hash,RS_hash,JS_hash,
-                   PJW_hash,ELF_hash,BKDR_hash,SDBM_hash,DJB_hash,AP_hash,CRC_hash);
+                              PJW_hash,ELF_hash,BKDR_hash,SDBM_hash,DJB_hash,AP_hash,CRC_hash);
     printf("init over\n");
     unsigned char * page = recv_page(len, sock);
     printf("recv_page ok\n");
@@ -47,5 +70,6 @@ int main(int argc, const char * argv[]) {
     printf("destory over\n");
     //BLOOM *b = bloom_init(BLOOM_SIZE, 10, hash_list);
     //shutdown(sock, SHUT_RDWR);
+    */
     return 0;
 }
